71081d94bb0e9c0ea92e19c29cf12f374bf298ac
Romain Fromi <romain.fromi@gmail.com>
2021-06-30 16:33:42 +0200

    Fix avatar display

--- app/src/board/AvatarPanel.tsx
+++ app/src/board/AvatarPanel.tsx
@@ -1,62 +1,51 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-import PlayerRole from "@gamepark/brigands/types/PlayerRole";
-import { Avatar, Player } from "@gamepark/react-client";
-import { FC } from "react";
-import { useTranslation } from "react-i18next";
-import Images from "../utils/Images";
+import {css} from '@emotion/react'
+import PlayerRole from '@gamepark/brigands/types/PlayerRole'
+import {Avatar, Player} from '@gamepark/react-client'
+import {SpeechBubbleDirection} from '@gamepark/react-client/dist/Avatar'
+import {FC} from 'react'
+import {useTranslation} from 'react-i18next'
+import Images from '../utils/Images'
 
 type Props = {
-    playerInfo : Player<PlayerRole> | undefined
-    role:PlayerRole
+  playerInfo: Player<PlayerRole> | undefined
+  role: PlayerRole
 }
 
-const AvatarPanel : FC<Props> = ({playerInfo, role}) => {
-
-    const {t} = useTranslation()
-
-    return(
-
-        <div css={avatarStyle}>
-            {playerInfo?.avatar 
-                ? <Avatar css={alternativeAvatarStyle} avatarStyle="Circle" {...playerInfo.avatar}/> 
-                : <img alt={t('Player avatar')} src={getAlternativeAvatar(role)} css={alternativeAvatarStyle} draggable={false}/>
-            }
-        </div>
-
-    )
-
+const AvatarPanel: FC<Props> = ({playerInfo, role}) => {
+  const {t} = useTranslation()
+  if (playerInfo?.avatar) {
+    return <Avatar css={avatarStyle} playerId={role} speechBubbleProps={{direction: SpeechBubbleDirection.BOTTOM_RIGHT}}/>
+  } else {
+    return <img alt={t('Player avatar')} src={getAlternativeAvatar(role)} css={avatarStyle} draggable={false}/>
+  }
 }
 
 const avatarStyle = css`
-border-radius:100%;
-float:left;
-margin:1em 1em;
-height:6em;
-width:6em;
+  position: relative;
+  float: left;
+  border-radius: 100%;
+  margin: 1em 1em;
+  height: 6em;
+  width: 6em;
+  color: black;
 `
 
-const alternativeAvatarStyle = css`
-width:100%;
-height:100%;
-border-radius:100%;
-`
-
-function getAlternativeAvatar(role:PlayerRole):string{
-    switch(role){
-        case PlayerRole.BlueThief :
-            return Images.tokenKickBlue
-        case PlayerRole.GreenThief :
-            return Images.tokenKickGreen
-        case PlayerRole.PurpleThief :
-            return Images.tokenKickPurple
-        case PlayerRole.RedThief :
-            return Images.tokenKickRed
-        case PlayerRole.YellowThief :
-            return Images.tokenKickYellow
-        default :
-            return Images.mercenary
-    }
+function getAlternativeAvatar(role: PlayerRole): string {
+  switch (role) {
+    case PlayerRole.BlueThief :
+      return Images.tokenKickBlue
+    case PlayerRole.GreenThief :
+      return Images.tokenKickGreen
+    case PlayerRole.PurpleThief :
+      return Images.tokenKickPurple
+    case PlayerRole.RedThief :
+      return Images.tokenKickRed
+    case PlayerRole.YellowThief :
+      return Images.tokenKickYellow
+    default :
+      return Images.mercenary
+  }
 }
 
 export default AvatarPanel
